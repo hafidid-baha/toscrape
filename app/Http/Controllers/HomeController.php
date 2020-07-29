2a28e08ab9d30ff6f26c2d00a9494385b780d6f1
hafid id-baha <hafidsoftdev@gmail.com>
2020-07-29 23:12:01 +0100

    upgrade the code and updating the layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,21 +10,56 @@ class HomeController extends Controller
 {
 	public function index()
 	{
-
+		
+		$data = [];
 		// get data from the first page
 		$crawler = Goutte::request('GET', 'http://quotes.toscrape.com');
-		$crawler->filter('.quote .text')->each(function ($node) {
-			echo $node->text().'<br />';
-		});
-
+		// push the first line to the data array
+		$this->fetchCrawler($crawler,$data);
+		// dd($data);
 		// get data from page 2 to 10
 		foreach (range(2, 10) as $l) {
 			// change the fetched url each time to get data from the next page
 			$crawler = Goutte::request('GET', 'http://quotes.toscrape.com/page/'.$l.'/');
-			$crawler->filter('.quote .text')->each(function ($node) {
-				// print each quotes text to the user
-				echo $node->text().'<br />';
+			$this->fetchCrawler($crawler,$data);
+		}	
+
+		
+		return view("welcome")->with(["data"=>$data]);
+	}
+
+	public function fetchCrawler($crawler,&$data){
+		$line = "";
+		// get the quote text
+		$crawler->filter('.quote')->each(function ($node) use(&$line,&$data){
+
+			$node->filter(".text")->each(function($node)use(&$line){
+				$line .= $node->text().";";
 			});
-		}		
+			$node->filter(".author")->each(function($node)use(&$line){
+				$line .= $node->text().";";
+			});
+			$node->filter(".tags .tag")->each(function($node)use(&$line){
+				$line .= $node->text().",";
+			});
+			array_push($data,$line);
+			$line = "";
+			
+		});
+		
+		// // get the guote author
+		// $crawler->filter('.quote .author')->each(function ($node) use(&$line) {
+		// 	// echo $node->text().'<br />';
+		// 	$line .= $node->text().";";
+		// 	// array_push($data,$node->text());
+		// });
+		// // get the guote tags
+		// $crawler->filter('.quote .tags .tag')->each(function ($node) use(&$line) {
+		// 	// echo $node->text().'<br />';
+		// 	$line .= $node->text().",";
+		// 	// array_push($data,$node->text());
+		// });
+
+		// return $line;
 	}
 }
