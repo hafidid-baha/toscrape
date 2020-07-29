8f3f809c6beac3221d1045956e61924df676b125
hafid id-baha <hafidsoftdev@gmail.com>
2020-07-29 22:00:44 +0100

    fetching data from pages from 1 to 10

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Weidner\Goutte\GoutteFacade as Goutte;
 
 class HomeController extends Controller
 {
-    public function index(){
-        
-        $crawler = Goutte::request('GET', 'http://quotes.toscrape.com/');
-        // $crawler->filter('.result__title .result__a')->each(function ($node) {
-        //   dd($node->text());
-        // });
-        $crawler->filter('.quote .text')->each(function ($node) {
-          dd($node->text());
-        });
+	public function index()
+	{
 
-        
-    }
+		// get data from the first page
+		$crawler = Goutte::request('GET', 'http://quotes.toscrape.com');
+		$crawler->filter('.quote .text')->each(function ($node) {
+			echo $node->text().'<br />';
+		});
+
+		// get data from page 2 to 10
+		foreach (range(2, 10) as $l) {
+			// change the fetched url each time to get data from the next page
+			$crawler = Goutte::request('GET', 'http://quotes.toscrape.com/page/'.$l.'/');
+			$crawler->filter('.quote .text')->each(function ($node) {
+				// print each quotes text to the user
+				echo $node->text().'<br />';
+			});
+		}		
+	}
 }
